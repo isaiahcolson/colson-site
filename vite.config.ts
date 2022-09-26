0ad8ae17d93f56e3ec90ba44dbcfd81ee3f968bc
Isaiah Colson <colson.isaiah@gmail.com>
2022-09-26 12:52:39 -0500

    setup mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mdx()],
 });
